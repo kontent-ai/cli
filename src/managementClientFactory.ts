21a1a0de15f9a4a1987e6427c8d94466d5e7b551
Ondrej Chrastina <chrastina.ondra@gmail.com>
2021-06-22 14:05:15 +0200

    upgrade backup manager to prevent version clashes
    picked from a4496fa (#23)

--- src/managementClientFactory.ts
+++ src/managementClientFactory.ts
@@ -15,28 +15,28 @@ const retryAbleCodes = [429, 503];
 dotEnv.config();
 
 export const createManagementClient = (params: ICreateManagementClientParams): ManagementClient => {
+    // TODO - verify whether to use custom implementation to be able to register axios interceptor just for logging
     const httpService = new HttpService({
-        requestInterceptor: (config) => {
-            config.maxContentLength = 104857600;
-            config.headers['X-KC-SOURCE'] = `${packageInfo.name};${packageInfo.version}`;
-
-            if (params.debugMode) {
-                console.group(chalk.bgCyan(chalk.yellowBright('Request details:')));
-                console.log(chalk.yellow('URL:'), config.url);
-                console.log(chalk.yellow('Method:'), config.method);
-                console.log(chalk.yellow('Body:'), config.data);
-                console.groupEnd();
-            }
-            return config;
-        },
-        responseInterceptor: (config) => {
-            if (params.debugMode) {
+        axiosRequestConfig: {
+            headers: {
+                'X-KC-SOURCE': `${packageInfo.name};${packageInfo.version}`,
+            },
+            transformRequest: (data: any, headers?: any) => {
+                if (params.debugMode) {
+                    console.group(chalk.bgCyan(chalk.yellowBright('Request details:')));
+                    console.log(chalk.yellow('Headers:'), headers);
+                    console.log(chalk.yellow('Body:'), data);
+                    console.groupEnd();
+                }
+                return data;
+            },
+            transformResponse: (data: any, headers?: any) => {
                 console.group(chalk.bgCyan(chalk.yellowBright('Response details:')));
-                console.error(chalk.yellow('Body:'), config.data);
+                console.log(chalk.yellow('Headers:'), headers);
+                console.log(chalk.yellow('Body:'), data);
                 console.groupEnd();
-            }
-
-            return config;
+                return data;
+            },
         },
     });
 
@@ -44,12 +44,10 @@ export const createManagementClient = (params: ICreateManagementClientParams): M
         httpService: httpService,
         projectId: params.projectId,
         apiKey: params.apiKey,
-        isDeveloperMode: false,
         baseUrl: process.env.BASE_URL,
         retryStrategy: {
             addJitter: true,
             deltaBackoffMs: 1000,
-            maxCumulativeWaitTimeMs: 60000,
             maxAttempts: 10,
             canRetryError: (error: any) => {
                 const timeout = error.errno === 'ETIMEDOUT';
