34aba661e0cab8c812ee080c38a41ae2ff1ce1d3
Ivan Kiral <kiralivan@gmail.com>
2023-03-30 11:31:05 +0200

    Add test for loading plugin

---
+++ src/tests/statusPlugin/statusPlugin.test.ts
@@ -0,0 +1,29 @@
+import { loadStatusPlugin } from "../../utils/status/statusPlugin";
+
+describe("status plugin tests", () => {
+
+    const saveStatus = () => {};
+    const readStatus = () => ({});
+
+    jest.mock('plugin', () => ({
+        saveStatus,
+        readStatus
+    }), {virtual:true});
+
+    jest.mock('malformedPlugin', () => ({
+        save: saveStatus,
+        read: readStatus
+    }), {virtual:true});
+    
+    it("test correct plugin", async () => {
+        const functions = await loadStatusPlugin('plugin');
+
+        expect(functions.saveStatus).toEqual(saveStatus);
+        expect(functions.readStatus).toEqual(readStatus);
+    })
+
+    it("test malformed plugin", async () => {
+        expect (loadStatusPlugin('malformedPlugin')).rejects.toThrow();
+    })
+
+})
