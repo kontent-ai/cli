90d8bbf80002c499bc8a2aea74063d40c8dbd11d
Ivan Kiral <kiralivan@gmail.com>
2023-03-31 10:18:00 +0200

    Add test for testing loadMigrationsExecutionStatus

--- src/tests/statusManager.test.ts
+++ src/tests/statusManager.test.ts
@@ -1,12 +1,14 @@
-import { markAsCompleted, wasSuccessfullyExecuted } from '../utils/statusManager';
-import { readFileSync } from 'fs';
+import * as statusManager from '../utils/statusManager';
+import * as statusPlugin from '../utils/status/statusPlugin';
+import * as fileUtils from '../utils/fileUtils';
+import * as fs from 'fs';
 import * as path from 'path';
 import { IStatus } from '../models/status';
 
 const readStatusFile = (): IStatus => {
     const statusFilepath = path.join(process.cwd(), 'status.json');
 
-    const fileContent = readFileSync(statusFilepath).toString();
+    const fileContent = fs.readFileSync(statusFilepath).toString();
     return JSON.parse(fileContent);
 };
 
@@ -18,7 +20,7 @@ describe('Status manager', () => {
     it('Project has success status in status manager file', async () => {
         const projectId = 'project1';
         const migrationName = 'migration1';
-        await markAsCompleted(projectId, migrationName, 1);
+        await statusManager.markAsCompleted(projectId, migrationName, 1);
 
         const statusFile = readStatusFile();
         const status = statusFile[projectId][0];
@@ -30,9 +32,9 @@ describe('Status manager', () => {
         const project1Id = 'project1';
         const project2Id = 'project2';
         const migration1Name = 'migration1';
-        await markAsCompleted(project1Id, migration1Name, 1);
+        await statusManager.markAsCompleted(project1Id, migration1Name, 1);
 
-        const projectMigrationStatus = wasSuccessfullyExecuted(migration1Name, project2Id);
+        const projectMigrationStatus = statusManager.wasSuccessfullyExecuted(migration1Name, project2Id);
 
         expect(projectMigrationStatus).toBe(false);
     });
@@ -40,10 +42,25 @@ describe('Status manager', () => {
     it('Executed migration is present in status file', async () => {
         const project2Id = 'project2';
         const migration2Name = 'migration2';
-        await markAsCompleted(project2Id, migration2Name, 1);
+        await statusManager.markAsCompleted(project2Id, migration2Name, 1);
 
-        const projectMigrationStatus = wasSuccessfullyExecuted(project2Id, migration2Name);
+        const projectMigrationStatus = statusManager.wasSuccessfullyExecuted(project2Id, migration2Name);
 
         expect(projectMigrationStatus).toBe(false);
+    });
+
+    it('loadMigrationsExecutionStatus to be called with plugins', async () => {
+        jest.spyOn(fileUtils, 'fileExists').mockReturnValue(true);
+
+        const readStatusMocked = jest.fn().mockResolvedValue({});
+
+        jest.spyOn(statusPlugin, 'loadStatusPlugin').mockResolvedValue({
+            readStatus: readStatusMocked,
+            saveStatus: jest.fn().mockImplementation(() => Promise.resolve()),
+        });
+
+        await statusManager.loadMigrationsExecutionStatus();
+
+        expect(readStatusMocked).toHaveBeenCalled();
     });
 });
