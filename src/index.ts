4752e375ff7b5c4bd22acd69792a634675dd132c
Ivan Kiral <kiralivan@gmail.com>
2024-04-29 12:12:15 +0200

    remove backup manager

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,6 @@ const createMigrationTool = (): number => {
         .example('kontent', 'migration run --name migration01 --environment DEV --debug true')
         .example('kontent', 'migration run --all --environment DEV')
         .example('kontent', 'migration run --range 1:4 --environment DEV')
-
-        .example('kontent', 'backup --action backup --environment-id <YOUR_ENVIRONMENT_ID> --api-key <YOUR_MANAGEMENT_API_KEY>')
-        .example('kontent', 'backup --action backup --environment <YOUR_ENVIRONMENT>')
-        .example('kontent', 'backup --action restore --name backup_file --environment-id <YOUR_ENVIRONMENT_ID> --api-key <YOUR_MANAGEMENT_API_KEY>')
-        .example('kontent', 'backup --action restore --name backup_file --environment <YOUR_ENVIRONMENT> --preserve-workflow false')
-        .example('kontent', 'backup --action clean --environment-id <YOUR_ENVIRONMENT_ID> --api-key <YOUR_MANAGEMENT_API_KEY>')
         .strict().argv;
 
     return 0;
